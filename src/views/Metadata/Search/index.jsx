2d8cfca86475bd2ff2f03c19bf676a9c7249f5af
lenolee16 <407641180@qq.com>
2019-01-03 16:23:30 +0800

    快捷搜索

--- src/views/Metadata/Search/index.jsx
+++ src/views/Metadata/Search/index.jsx
@@ -11,41 +11,64 @@ class MetadataSearch extends PureComponent {
     super(props)
     this.state = {
       data: [],
+      tableName: '',
+      fieldName: '',
+      loading: false,
       isSearched: false
     }
+  }
+  search = () => {
+    const { tableName, fieldName } = this.state
+    if (!tableName && !fieldName) {
+      return window._message.error('请输入至少一项搜索条件')
+    }
+    this.setState({ loading: true })
+    window._http.post('/metadata/targetField/searchField', { tableName, fieldName }).then(res => {
+      if (res.data.code === 0) {
+        this.setState({
+          data: res.data.data,
+          isSearched: true,
+          loading: false
+        })
+      } else {
+        this.setState({ loading: false })
+      }
+    }).catch(res => {
+      this.setState({ loading: false })
+    })
   }
   render () {
     return (
       <div className='MetadataSearch'>
         <Card title='数据源搜索'>
           <div className='searchForm'>
-            <Input addonBefore='表名' size='large' placeholder='请输入数据表名' />
-            <Input addonBefore='字段名' size='large' placeholder='请输入字段名' />
-            <Button type='primary' size='large' icon='search'>搜索</Button>
+            <Input addonBefore='表名' size='large' value={this.state.tableName} onChange={(e) => this.setState({ tableName: e.target.value })} placeholder='请输入数据表名' />
+            <Input addonBefore='字段名' size='large' value={this.state.fieldName} onChange={(e) => this.setState({ fieldName: e.target.value })} placeholder='请输入字段名' />
+            <Button type='primary' size='large' icon='search' onClick={this.search} loading={this.state.loading}>搜索</Button>
           </div>
           {this.state.isSearched ? <div className='searchResult'>
-            <Table dataSource={this.state.data}>
+            <Table dataSource={this.state.data} rowKey='targetTableId' loading={this.state.loading}>
               <Column
                 title='数据源名'
-                dataIndex='title'
-                key='title'
-              />
-              <Column
-                title='数据库名'
-                dataIndex='name'
-                key='name'
+                dataIndex='dataSourceName'
+                key='dataSourceName'
               />
               <Column
                 title='数据表名'
-                dataIndex='des'
-                key='des'
+                dataIndex='targetTableName'
+                key='targetTableName'
+              />
+              <Column
+                title='数据字段名'
+                dataIndex='targetFieldName'
+                key='targetFieldName'
               />
               <Column
                 title='操作'
                 key='action'
                 render={(text, record) => (
                   <span>
-                    <Link to={`/metadataList/${record.id}/${record.id}/${record.id}`}>查看</Link>
+                    <Link to={`/metadataList/${record.dataSourceId}/${record.targetTableId}${record.targetFieldId ? ('/' + record.targetFieldId) : record.targetFieldId}`}>查看</Link>
                   </span>
                 )}
               />
