ac7cd9ea2b72578dc28bcaaac8c47c868c47b939
lenolee16 <407641180@qq.com>
2019-01-03 17:21:07 +0800

    增加全局通知方法

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import http from 'utils/http'
-import { message } from 'antd'
+import { message, notification } from 'antd'
 
 window._http = http
 window._message = message
+window._notification = notification
 
 ReactDOM.render(
   <App />,
