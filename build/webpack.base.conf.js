420858f93ec72c49b9e8b1cd6f39fe5d16b99ace
lenolee16 <407641180@qq.com>
2019-01-07 20:55:45 +0800

     修改配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -56,7 +56,8 @@ module.exports = {
                 loader: 'url-loader',
                 query: {
                     limit: 10000,
-                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                    name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+                    outputPath: '../../'
                 }
             },
             {
