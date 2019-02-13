bd16265c1472c59d3dce7def24851275947ea686
lenolee16 <407641180@qq.com>
2019-02-13 14:31:27 +0800

    修改权限高级组件

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types'
 import { withRouter } from 'react-router-dom'
 import utils from './index'
 
-const validate = function (history) {
+import { Redirect } from 'react-router'
+
+const isValidate = function (history) {
   const isLoggedIn = utils.getLoggedIn()
-  if (!isLoggedIn && history.location.pathname !== '/login') {
-    history.replace('/login')
-  }
+  return isLoggedIn || history.location.pathname === '/login'
 }
 
 /**
@@ -15,24 +15,18 @@ const validate = function (history) {
  */
 export default function authHOC (BaseComponent) {
   class Restricted extends Component {
-    componentWillMount () {
-      this.checkAuthentication(this.props)
-    }
-    componentWillReceiveProps (nextProps) {
-      if (nextProps.location !== this.props.location) {
-        this.checkAuthentication(nextProps)
+    render () {
+      if (isValidate(this.props.history)) {
+        return <BaseComponent {...this.props} />
+      } else {
+        return <Redirect to='/login' />
       }
     }
-    checkAuthentication (params) {
-      const { history } = params
-      validate(history)
-    }
-    render () {
-      return <BaseComponent {...this.props} />
-    }
   }
+
   Restricted.propTypes = {
-    location: PropTypes.object
+    history: PropTypes.object
   }
+
   return withRouter(Restricted)
 }
